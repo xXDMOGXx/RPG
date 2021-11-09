73a137025cd1f696f7be368d5a3c4f2447ee9d16
xXDMOGXx <xxdmogxx@gmail.com>
2021-11-09 09:45:08 -0600

    Additional optimizations and beginning of load/play

--- src/com/rpgmain/controllers/MainController.java
+++ src/com/rpgmain/controllers/MainController.java
@@ -3,11 +3,20 @@ package com.rpgmain.controllers;
 import javax.swing.*;
 
 public class MainController {
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(MainController::StartGame);
+        SwingUtilities.invokeLater(MainController::startGame);
     }
 
-    private static void StartGame() {
-        WindowController.StartWindow(new MainMenuPanel());
+    private static void startGame() {
+        WindowController.startWindow(new MainMenuPanel());
+    }
+
+    public static void newGame() {
+
+    }
+
+    public static void playGame(int saveSlot) {
+
     }
 }
--- src/com/rpgmain/controllers/WindowController.java
+++ src/com/rpgmain/controllers/WindowController.java
@@ -11,31 +11,35 @@ import java.awt.*;
 
 public class WindowController {
     private static final JFrame frame = new JFrame("Generic RPG");
+    // Window Size is 1248 x 720
 
-    public static void StartWindow(JPanel panel) {
-        frame.setSize(1248, 720);
+    public static void startWindow(JPanel panel) {
         frame.add(panel);
+        frame.setResizable(false);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
-    public static void ChangeWindow(JPanel panel) {
+    public static void changeWindow(JPanel panel) {
         frame.getContentPane().removeAll();
         frame.add(panel);
         frame.pack();
         frame.repaint();
     }
 
-    public static void CloseWindow() {
+    public static void closeWindow() {
         frame.dispose();
     }
 }
 
-class MainMenuPanel extends JPanel{
+class MainMenuPanel extends JPanel {
     public MainMenuPanel() {
+        initUI();
+    }
+
+    private void initUI() {
         BoxLayout boxlayout = new BoxLayout(this, BoxLayout.Y_AXIS);
         setLayout(boxlayout);
         setBorder(new EmptyBorder(new Insets(75, 0, 70, 0)));
@@ -44,10 +48,10 @@ class MainMenuPanel extends JPanel{
         title.setPreferredSize(new Dimension(300, 50));
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        JButton playButton = new JButton("Play");
-        playButton.setPreferredSize(new Dimension(300, 75));
-        playButton.setMaximumSize(playButton.getPreferredSize());
-        playButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JButton newButton = new JButton("New Game");
+        newButton.setPreferredSize(new Dimension(300, 75));
+        newButton.setMaximumSize(newButton.getPreferredSize());
+        newButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         JButton loadButton = new JButton("Load");
         loadButton.setPreferredSize(new Dimension(300, 75));
@@ -68,7 +72,7 @@ class MainMenuPanel extends JPanel{
         add(title);
         add(Box.createRigidArea(new Dimension(0, 150)));
         add(Box.createVerticalGlue());
-        add(playButton);
+        add(newButton);
         add(Box.createRigidArea(new Dimension(0, 25)));
         add(Box.createVerticalGlue());
         add(loadButton);
@@ -79,24 +83,57 @@ class MainMenuPanel extends JPanel{
         add(Box.createVerticalGlue());
         add(quitButton);
 
-        playButton.addActionListener(e -> {});
-        loadButton.addActionListener(e -> WindowController.ChangeWindow(new LoadMenuPanel()));
-        settingsButton.addActionListener(e -> WindowController.ChangeWindow(new SettingsMenuPanel()));
-        quitButton.addActionListener(e -> WindowController.CloseWindow());
+        newButton.addActionListener(e -> MainController.newGame());
+        loadButton.addActionListener(e -> WindowController.changeWindow(new LoadMenuPanel()));
+        settingsButton.addActionListener(e -> WindowController.changeWindow(new SettingsMenuPanel()));
+        quitButton.addActionListener(e -> WindowController.closeWindow());
     }
 }
 
-class LoadMenuPanel extends JPanel{
+class MainGamePanel extends JPanel {
+    public MainGamePanel() {
+        initUI();
+    }
+
+    private void initUI() {
+    }
+}
+
+class LoadMenuPanel extends JPanel {
     public LoadMenuPanel() {
+        initUI();
+    }
+
+    private void initUI() {
         BoxLayout boxlayout = new BoxLayout(this, BoxLayout.Y_AXIS);
         setLayout(boxlayout);
-        setBorder(new EmptyBorder(new Insets(75, 0, 370, 0)));
+        setBorder(new EmptyBorder(new Insets(75, 0, 70, 0)));
 
         JLabel title = new JLabel("Load Menu");
         title.setPreferredSize(new Dimension(300, 50));
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        JButton menuButton = new JButton("Main Menu");
+        JButton save1Button = new JButton("Save Slot 1");
+        save1Button.setPreferredSize(new Dimension(300, 75));
+        save1Button.setMaximumSize(save1Button.getPreferredSize());
+        save1Button.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton save2Button = new JButton("Save Slot 1");
+        save2Button.setPreferredSize(new Dimension(300, 75));
+        save2Button.setMaximumSize(save2Button.getPreferredSize());
+        save2Button.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton save3Button = new JButton("Save Slot 1");
+        save3Button.setPreferredSize(new Dimension(300, 75));
+        save3Button.setMaximumSize(save3Button.getPreferredSize());
+        save3Button.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton save4Button = new JButton("Save Slot 1");
+        save4Button.setPreferredSize(new Dimension(300, 75));
+        save4Button.setMaximumSize(save4Button.getPreferredSize());
+        save4Button.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton menuButton = new JButton("Back");
         menuButton.setPreferredSize(new Dimension(300, 75));
         menuButton.setMaximumSize(menuButton.getPreferredSize());
         menuButton.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -105,14 +142,30 @@ class LoadMenuPanel extends JPanel{
         add(title);
         add(Box.createRigidArea(new Dimension(0, 150)));
         add(Box.createVerticalGlue());
+        add(save1Button);
+        add(Box.createVerticalGlue());
+        add(save2Button);
+        add(Box.createVerticalGlue());
+        add(save3Button);
+        add(Box.createVerticalGlue());
+        add(save4Button);
+        add(Box.createVerticalGlue());
         add(menuButton);
 
-        menuButton.addActionListener(e -> WindowController.ChangeWindow(new MainMenuPanel()));
+        save1Button.addActionListener(e -> MainController.playGame(1));
+        save2Button.addActionListener(e -> MainController.playGame(2));
+        save3Button.addActionListener(e -> MainController.playGame(3));
+        save4Button.addActionListener(e -> MainController.playGame(4));
+        menuButton.addActionListener(e -> WindowController.changeWindow(new MainMenuPanel()));
     }
 }
 
-class SettingsMenuPanel extends JPanel{
+class SettingsMenuPanel extends JPanel {
     public SettingsMenuPanel() {
+        initUI();
+    }
+
+    private void initUI() {
         BoxLayout boxlayout = new BoxLayout(this, BoxLayout.Y_AXIS);
         setLayout(boxlayout);
         setBorder(new EmptyBorder(new Insets(75, 0, 370, 0)));
@@ -132,6 +185,6 @@ class SettingsMenuPanel extends JPanel{
         add(Box.createVerticalGlue());
         add(menuButton);
 
-        menuButton.addActionListener(e -> WindowController.ChangeWindow(new MainMenuPanel()));
+        menuButton.addActionListener(e -> WindowController.changeWindow(new MainMenuPanel()));
     }
 }
